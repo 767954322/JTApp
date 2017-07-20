3045ec0f0f45f817581005a88ed570338150a50e
gumenghao <menghao.gu@leediancn.com>
2017-07-20 16:45:33 +0800

    添加收藏文章

--- app/src/main/java/com/homechart/app/home/activity/ArticleDetailsActivity.java
+++ app/src/main/java/com/homechart/app/home/activity/ArticleDetailsActivity.java
@@ -5,8 +5,17 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
 import com.homechart.app.R;
+import com.homechart.app.commont.ClassConstant;
 import com.homechart.app.home.base.BaseActivity;
+import com.homechart.app.utils.CustomProgress;
+import com.homechart.app.utils.ToastUtils;
+import com.homechart.app.utils.volley.MyHttpManager;
+import com.homechart.app.utils.volley.OkStringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by gumenghao on 17/7/18.
@@ -28,7 +37,7 @@ public class ArticleDetailsActivity
     @Override
     protected void initExtraBundle() {
         super.initExtraBundle();
-        article_id =  getIntent().getStringExtra("article_id");
+        article_id = getIntent().getStringExtra("article_id");
     }
 
     @Override
@@ -46,11 +55,37 @@ public class ArticleDetailsActivity
     @Override
     protected void initData(Bundle savedInstanceState) {
         tv_tital_comment.setText("文章详情");
+
+
+        OkStringRequest.OKResponseCallback callBack = new OkStringRequest.OKResponseCallback() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+            }
+
+            @Override
+            public void onResponse(String s) {
+                try {
+                    JSONObject jsonObject = new JSONObject(s);
+                    int error_code = jsonObject.getInt(ClassConstant.Parame.ERROR_CODE);
+                    String error_msg = jsonObject.getString(ClassConstant.Parame.ERROR_MSG);
+                    String data_msg = jsonObject.getString(ClassConstant.Parame.DATA);
+                    if (error_code == 0) {
+                        ToastUtils.showCenter(ArticleDetailsActivity.this, "收藏成功");
+                    } else {
+                        ToastUtils.showCenter(ArticleDetailsActivity.this, "收藏失败");
+                    }
+                } catch (JSONException e) {
+                    ToastUtils.showCenter(ArticleDetailsActivity.this, "收藏失败");
+                }
+            }
+        };
+        MyHttpManager.getInstance().addShouCangArticle(article_id, callBack);
+
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.nav_left_imageButton:
                 ArticleDetailsActivity.this.finish();
                 break;
