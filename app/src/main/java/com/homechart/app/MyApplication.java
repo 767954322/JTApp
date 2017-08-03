808b7652e3550951d43647efd12ed3186ea3d9c8
gumenghao <menghao.gu@leediancn.com>
2017-08-03 11:52:58 +0800

    恢复原来的ga代码

--- app/src/main/java/com/homechart/app/MyApplication.java
+++ app/src/main/java/com/homechart/app/MyApplication.java
@@ -34,7 +34,6 @@ public class MyApplication extends Application {
     private static MyApplication myApplication;
     public static RequestQueue queue;
     private Tracker mTracker;
-    private GoogleAnalytics analytics;
 
     @Override
     public void onCreate() {
@@ -44,7 +43,6 @@ public class MyApplication extends Application {
             StrictMode.setVmPolicy(builder.build());
         }
         myApplication = this;
-        analytics = GoogleAnalytics.getInstance(this);
         queue = Volley.newRequestQueue(this);
         initJPush();
         initImageLoader();
@@ -116,6 +114,7 @@ public class MyApplication extends Application {
      */
     synchronized public Tracker getDefaultTracker() {
         if (mTracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
             // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
             mTracker = analytics.newTracker(R.xml.global_tracker);
         }
