8a044dc213a82ff8cc8bb22e10c08553319db9a6
gumenghao <menghao.gu@leediancn.com>
2017-08-08 09:54:33 +0800

    测试迭代2

--- app/src/main/java/com/homechart/app/home/activity/WelcomeActivity.java
+++ app/src/main/java/com/homechart/app/home/activity/WelcomeActivity.java
@@ -89,6 +89,7 @@ public class WelcomeActivity extends BaseActivity implements WelcomePagerAdapter
         adList.add(R.drawable.a1);
         adList.add(R.drawable.a2);
         adList.add(R.drawable.a3);
+
         return adList;
     }
 
