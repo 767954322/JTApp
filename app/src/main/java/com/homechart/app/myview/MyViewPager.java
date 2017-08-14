353e50d2278fc82f90e5bf483270b410d99fc29a
gumenghao <menghao.gu@leediancn.com>
2017-08-14 10:59:12 +0800

    添加viewpager滑动

--- app/src/main/java/com/homechart/app/myview/MyViewPager.java
+++ app/src/main/java/com/homechart/app/myview/MyViewPager.java
@@ -3,6 +3,7 @@ package com.homechart.app.myview;
 import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 /**
  * Created by Administrator on 2017/3/10/010.
@@ -10,6 +11,7 @@ import android.util.AttributeSet;
 
 public class MyViewPager extends ViewPager {
 
+    private boolean isCanScroll = true;
     public MyViewPager(Context context) {
         super(context);
     }
@@ -25,6 +27,29 @@ public class MyViewPager extends ViewPager {
     public void setCurrentItem(int item) {
         //super.setCurrentItem(item);源码
         super.setCurrentItem(item, false);//false表示切换的时候,不经过两个页面的中间页
+    }
+
+
+    public void setScanScroll(boolean isCanScroll) {
+        this.isCanScroll = isCanScroll;
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        /* return false;//super.onTouchEvent(arg0); */
+        if (isCanScroll)
+            return false;
+        else
+            return super.onTouchEvent(arg0);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        if (isCanScroll)
+            return false;
+        else
+            return super.onInterceptTouchEvent(arg0);
     }
 
 }
