639a2be61e4fbd00c188f340dadae4cb1bf2c907
gumenghao <menghao.gu@leediancn.com>
2017-11-02 11:21:49 +0800

    添加放大缩小

--- app/src/main/java/com/homechart/app/visearch/NewSearchResultActivity.java
+++ app/src/main/java/com/homechart/app/visearch/NewSearchResultActivity.java
@@ -1,10 +1,13 @@
 package com.homechart.app.visearch;
 
 import android.content.Intent;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.homechart.app.R;
@@ -13,6 +16,9 @@ import com.homechart.app.croplayout.EditableImage;
 import com.homechart.app.croplayout.NewEditPhotoViewMore;
 import com.homechart.app.home.base.BaseActivity;
 import com.homechart.app.home.bean.searchfservice.SearchSBean;
+import com.homechart.app.imagedetail.PhotoView;
+import com.homechart.app.imagedetail.PhotoViewAttacher;
+import com.homechart.app.utils.imageloader.ImageUtils;
 
 import java.io.Serializable;
 
@@ -30,11 +36,9 @@ public class NewSearchResultActivity
     private String imagePath;
     private String searchstatus;
     private SearchSBean searchSBean;
-    private NewEditPhotoViewMore mPhotoImage;
-    private EditableImage editableImage;
-    private FrameLayout fl_image;
     private int widerImage;
     private int heightImage;
+    private PhotoView pv_Image;
 
     @Override
     protected int getLayoutResId() {
@@ -56,10 +60,9 @@ public class NewSearchResultActivity
     @Override
     protected void initView() {
 
-        fl_image = (FrameLayout) findViewById(R.id.fl_image);
+        pv_Image = (PhotoView) findViewById(R.id.pv_Image);
         mTital = (TextView) findViewById(R.id.tv_tital_comment);
         mBackButton = (ImageButton) findViewById(R.id.nav_left_imageButton);
-        mPhotoImage = (NewEditPhotoViewMore) findViewById(R.id.photoedit_image_view);
 
     }
 
@@ -67,15 +70,20 @@ public class NewSearchResultActivity
     protected void initListener() {
         super.initListener();
         mBackButton.setOnClickListener(this);
+        pv_Image.setOnScaleChangeListener(new PhotoViewAttacher.OnScaleChangeListener() {
+            @Override
+            public void onScaleChange(float scaleFactor, float focusX, float focusY) {
+                Log.d("test", scaleFactor + "---" + focusX + "---" + focusY);
+            }
+        });
     }
 
     @Override
     protected void initData(Bundle savedInstanceState) {
         mTital.setText("自定义识别");
 
-        if(network.equals("true")){
-            editableImage = new EditableImage(imagePath, true);
-            mPhotoImage.initView(NewSearchResultActivity.this, editableImage, true);
+        if (network.equals("true")) {
+            ImageUtils.disBlackImage(imagePath, pv_Image);
         }
 
     }
