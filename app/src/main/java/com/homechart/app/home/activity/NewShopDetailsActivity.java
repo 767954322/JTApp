be295b7e6fef0da7e71d648b7aa593aa03bb090e
gumenghao <menghao.gu@leediancn.com>
2017-11-09 15:19:41 +0800

    添加商品详情

--- app/src/main/java/com/homechart/app/home/activity/NewShopDetailsActivity.java
+++ app/src/main/java/com/homechart/app/home/activity/NewShopDetailsActivity.java
@@ -24,6 +24,7 @@ public class NewShopDetailsActivity
     private ImageView iv_crop_imageview;
     private TextView tv_tital_comment;
     private String cropImage;
+    private ImageView iv_xuanxiang;
 
     @Override
     protected int getLayoutResId() {
@@ -36,12 +37,14 @@ public class NewShopDetailsActivity
         nav_left_imageButton = (ImageButton) findViewById(R.id.nav_left_imageButton);
         iv_crop_imageview = (ImageView) findViewById(R.id.iv_crop_imageview);
         tv_tital_comment = (TextView) findViewById(R.id.tv_tital_comment);
+        iv_xuanxiang = (ImageView) findViewById(R.id.iv_xuanxiang);
     }
 
     @Override
     protected void initListener() {
         super.initListener();
         nav_left_imageButton.setOnClickListener(this);
+        iv_xuanxiang.setOnClickListener(this);
     }
 
     @Override
@@ -61,6 +64,9 @@ public class NewShopDetailsActivity
         switch (v.getId()) {
             case R.id.nav_left_imageButton:
                 NewShopDetailsActivity.this.finish();
+                break;
+            case R.id.iv_xuanxiang:
+
                 break;
         }
 
