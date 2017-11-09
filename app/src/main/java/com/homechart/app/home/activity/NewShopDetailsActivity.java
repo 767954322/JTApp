6f87761ab8ec8cc70b3d959ec5cda0dd60c8a78e
gumenghao <menghao.gu@leediancn.com>
2017-11-09 18:28:38 +0800

    修改相似商品

--- app/src/main/java/com/homechart/app/home/activity/NewShopDetailsActivity.java
+++ app/src/main/java/com/homechart/app/home/activity/NewShopDetailsActivity.java
@@ -33,6 +33,18 @@ public class NewShopDetailsActivity
     private TextView tv_price_set;
     private TextView tv_guanjianzi_set;
     private TextView tv_type_set;
+    private RelativeLayout rl_price;
+    private ImageView iv_price_icon;
+    private TextView tv_price;
+    private ImageView iv_price_delect;
+    private RelativeLayout rl_type;
+    private ImageView iv_type_icon;
+    private TextView tv_type;
+    private ImageView iv_type_delect;
+    private RelativeLayout rl_guanjianzi;
+    private ImageView iv_guanjianzi_icon;
+    private TextView tv_guanjianzi;
+    private ImageView iv_guanjianzi_delect;
 
     @Override
     protected int getLayoutResId() {
@@ -54,12 +66,31 @@ public class NewShopDetailsActivity
         tv_type_set = (TextView) findViewById(R.id.tv_type_set);
         tv_guanjianzi_set = (TextView) findViewById(R.id.tv_guanjianzi_set);
 
+
+        rl_price = (RelativeLayout) findViewById(R.id.rl_price);
+        iv_price_icon = (ImageView) findViewById(R.id.iv_price_icon);
+        tv_price = (TextView) findViewById(R.id.tv_price);
+        iv_price_delect = (ImageView) findViewById(R.id.iv_price_delect);
+
+        rl_type = (RelativeLayout) findViewById(R.id.rl_type);
+        iv_type_icon = (ImageView) findViewById(R.id.iv_type_icon);
+        tv_type = (TextView) findViewById(R.id.tv_type);
+        iv_type_delect = (ImageView) findViewById(R.id.iv_type_delect);
+
+        rl_guanjianzi = (RelativeLayout) findViewById(R.id.rl_guanjianzi);
+        iv_guanjianzi_icon = (ImageView) findViewById(R.id.iv_guanjianzi_icon);
+        tv_guanjianzi = (TextView) findViewById(R.id.tv_guanjianzi);
+        iv_guanjianzi_delect = (ImageView) findViewById(R.id.iv_guanjianzi_delect);
+
     }
 
     @Override
     protected void initListener() {
         super.initListener();
         nav_left_imageButton.setOnClickListener(this);
+        iv_price_delect.setOnClickListener(this);
+        iv_type_delect.setOnClickListener(this);
+        iv_guanjianzi_delect.setOnClickListener(this);
         iv_xuanxiang.setOnClickListener(this);
         iv_close_set.setOnClickListener(this);
         tv_price_set.setOnClickListener(this);
@@ -98,24 +129,37 @@ public class NewShopDetailsActivity
 //                rl_set_shuaixuan.setAnimation(AnimationUtils.makeOutAnimation(this, true));
                 break;
             case R.id.tv_price_set:
+                rl_price.setVisibility(View.VISIBLE);
                 rl_add_shuaixuan.setVisibility(View.VISIBLE);
                 rl_set_shuaixuan.setVisibility(View.GONE);
                 rl_add_shuaixuan.setAnimation(AnimationUtils.makeInAnimation(this, true));
 //                rl_set_shuaixuan.setAnimation(AnimationUtils.makeOutAnimation(this, true));
                 break;
             case R.id.tv_type_set:
+                rl_type.setVisibility(View.VISIBLE);
                 rl_add_shuaixuan.setVisibility(View.VISIBLE);
                 rl_set_shuaixuan.setVisibility(View.GONE);
                 rl_add_shuaixuan.setAnimation(AnimationUtils.makeInAnimation(this, true));
 //                rl_set_shuaixuan.setAnimation(AnimationUtils.makeOutAnimation(this, true));
                 break;
             case R.id.tv_guanjianzi_set:
+                rl_guanjianzi.setVisibility(View.VISIBLE);
                 rl_add_shuaixuan.setVisibility(View.VISIBLE);
                 rl_set_shuaixuan.setVisibility(View.GONE);
                 rl_add_shuaixuan.setAnimation(AnimationUtils.makeInAnimation(this, true));
 //                rl_set_shuaixuan.setAnimation(AnimationUtils.makeOutAnimation(this, true));
                 break;
+            case R.id.iv_price_delect:
+                rl_price.setVisibility(View.GONE);
+                break;
+            case R.id.iv_type_delect:
+                rl_type.setVisibility(View.GONE);
+                break;
+            case R.id.iv_guanjianzi_delect:
+                rl_guanjianzi.setVisibility(View.GONE);
+                break;
         }
 
     }
+
 }
