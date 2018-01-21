ebc618fdcfa1a7347dd359eed5864c510289991a
gumenghao <menghao.gu@leediancn.com>
2018-01-21 11:28:20 +0800

    修改bug 和版本号

--- app/src/main/java/com/homechart/app/home/activity/NewHuoDongDetailsActivity.java
+++ app/src/main/java/com/homechart/app/home/activity/NewHuoDongDetailsActivity.java
@@ -319,6 +319,7 @@ public class NewHuoDongDetailsActivity
                     public void onClick(View v) {
                         Intent intent = new Intent(NewHuoDongDetailsActivity.this, InspirationDetailActivity.class);
                         intent.putExtra("user_id", mUserId);
+                        intent.putExtra("ifHideEdit", true);
                         intent.putExtra("tag", "true");
                         intent.putExtra("album_id", mListData.get(position).getAlbum_info().getAlbum_id());
                         startActivityForResult(intent, 2);
