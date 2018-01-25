c6caee6404a8f07fc3ef25c33778bf1e15207e6b
gumenghao <menghao.gu@leediancn.com>
2018-01-25 11:54:02 +0800

    修改文案

--- app/src/main/java/com/homechart/app/home/activity/NewHuoDongDetailsActivity.java
+++ app/src/main/java/com/homechart/app/home/activity/NewHuoDongDetailsActivity.java
@@ -303,7 +303,7 @@ public class NewHuoDongDetailsActivity
             public void convert(BaseViewHolder holder, final int position) {
                 ((TextView) holder.getView(R.id.tv_item_name)).setText(mListData.get(position).getAlbum_info().getAlbum_name());
                 ((TextView) holder.getView(R.id.tv_item_num_pic)).setText(mListData.get(position).getAlbum_info().getItem_num() + "张");
-                ((TextView) holder.getView(R.id.tv_item_num_dingyue)).setText(mListData.get(position).getAlbum_info().getSubscribe_num() + "订阅");
+                ((TextView) holder.getView(R.id.tv_item_num_dingyue)).setText(mListData.get(position).getAlbum_info().getSubscribe_num() + "收藏");
                 RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) holder.getView(R.id.iv_item_pic).getLayoutParams();
                 layoutParams.height = widthPic;
                 layoutParams.width = widthPic;
